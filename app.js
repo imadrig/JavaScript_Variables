4f1144f5198c001a43a7c7ac58bde1fcecfee314
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-09-20 20:39:54 -0400

    Completed remainder of exercise

--- app.js
+++ app.js
@@ -10,7 +10,34 @@ var age = 89;
 // Exercise 2
 const firstName = "Jane";
 
+console.log(`${firstName} ${lastName} is ${age} years old.`)
+
 // Exercise 3
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+
+console.log(`${language} was first released in ${createdYear}.`);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(isOnSale);
+console.log(price);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`${author} wrote ${title}.`);
+
+
+
